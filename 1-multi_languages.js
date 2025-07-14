76903d71ca4c915b7bb058533b9b56d155807287
Beatrice Labe Lary <beatricelabelary@gmail.com>
2025-07-14 12:08:20 +0100

    Printing many sentences

---
+++ 1-multi_languages.js
@@ -0,0 +1,9 @@
+//variables
+const sentence1 = "C is fun";
+const sentence2 = "Python is cool";
+const sentence3 = "JavaScript is amazing";
+
+//output
+console.log(sentence1);
+console.log(sentence2);
+console.log(sentence3);
