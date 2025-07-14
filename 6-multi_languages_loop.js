08c19a44a2ed7d418818488e826dcfa54a0064aa
Beatrice Labe Lary <beatricelabelary@gmail.com>
2025-07-14 15:04:30 +0100

    looping through a array

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 const arrayOfStrings = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
 //printing
-for(let i = 0; i < 3; i++){
+for (let i = 0; i < arrayOfStrings.length; i++){
     console.log(arrayOfStrings[i]);
 }
